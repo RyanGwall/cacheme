5755d31594675593b34c6a8e7916c8de9945b70a
Ryan Grewal <ryansgrewal@gmail.com>
2024-04-22 11:50:14 -0600

    Added another bug fix for database connection

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const morgan = require('morgan');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo')(session);
-// const dbUrl = process.env.DB_URL;
-const dbUrl = 'mongodb://127.0.0.1:27017/cache-me';
+const dbUrl = process.env.DB_URL;
+// const dbUrl = 'mongodb://127.0.0.1:27017/cache-me';
 
 
 const geocacheRoutes = require('./routes/geocaches');
