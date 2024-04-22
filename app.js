38bb38df1c91842a35934ff6ecab4e4e5fa4d760
Ryan Grewal <ryansgrewal@gmail.com>
2024-04-22 12:47:42 -0600

    Fixed another seeding bug

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const morgan = require('morgan');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo')(session);
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/cache-me';
 // const dbUrl = 'mongodb://localhost:27017/cache-me';
 
 
--- seeds/index.js
+++ seeds/index.js
@@ -2,14 +2,9 @@ const mongoose = require('mongoose');
 const cities = require('./cities');
 const { items, descriptors } = require('./seedHelpers');
 const Geocache = require('../models/geocache');
-const dbUrl = process.env.DB_URL;
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/cache-me';
 
-mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-});
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
