6e495a20fc5ecee3f53eb0c44d1ff0c91adba2ee
Ryan Grewal <ryansgrewal@gmail.com>
2024-04-22 11:51:10 -0600

    Changed local database variable back to original value

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo')(session);
 const dbUrl = process.env.DB_URL;
-// const dbUrl = 'mongodb://127.0.0.1:27017/cache-me';
+// const dbUrl = 'mongodb://localhost:27017/cache-me';
 
 
 const geocacheRoutes = require('./routes/geocaches');
